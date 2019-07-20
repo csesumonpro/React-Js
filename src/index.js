97d58b524e00aec70e92e5adc2014df372fcf40d
Sumon sarker <csesumonpro@gmail.com>
2019-07-20 12:32:54 +0600

    state and lifecycle basic system

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Clock extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            date : new Date()
+        }
+    }
+    componentDidMount(){
+       this.timer =  setInterval(()=>this.startTimer(),1000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.timer)
+    }
+    startTimer(){
+        this.setState({
+            date: new Date()
+        })
+    }
+    render(){
+        return <h2>Time is : {this.state.date.toLocaleTimeString()}</h2>
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    ReactDOM.render(
+        <Clock/>,
+        document.getElementById('root')
+    );
+
+
